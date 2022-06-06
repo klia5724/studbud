74ad1049c4e9bf4d25f74c1d1f956fe13fc92a17
Kelly Liang <pip2363@gmail.com>
2022-06-06 15:32:57 +1000

    kanban board drag and drop + fix sidebar

--- public/script.js
+++ public/script.js
@@ -1,3 +1,31 @@
+// https://www.w3schools.com/jsref/event_ondragstart.asp
+// kanban board - drag and drop tasks
+document.addEventListener("dragstart", function (event) {
+    event.dataTransfer.setData("task", event.target.id);
+});
+
+document.addEventListener("dragover", function (event) {
+    event.preventDefault();
+});
+
+document.addEventListener("drop", function (event) {
+    event.preventDefault();
+    if (event.target.className == "taskCol") {
+        const data = event.dataTransfer.getData("task");
+        event.target.appendChild(document.getElementById(data));
+    }
+});
+
+// delete tasks
+const taskDelete = document.querySelectorAll(".delete");
+
+taskDelete.forEach((x) => {
+    x.addEventListener("click", () => {
+        x.parentElement.style.display = "none";
+    });
+});
+
+
 // open and close sidebar functions following W3 School How TO - Collapse Sidebar Tutorial:
 // https://www.w3schools.com/howto/howto_js_collapse_sidebar.asp 
 // REMEMBER TO APA REFERENCE^^^
