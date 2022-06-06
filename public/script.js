5facb1ee8b0808b125c53c3cd5875133b6b34745
Kelly Liang <pip2363@gmail.com>
2022-06-06 23:03:51 +1000

    add code comments

--- public/script.js
+++ public/script.js
@@ -1,13 +1,14 @@
-// https://www.w3schools.com/jsref/event_ondragstart.asp
-// kanban board - drag and drop tasks
+// kanban board - use event listeners to drag and drop tasks
+// track when task card is dragged
 document.addEventListener("dragstart", function (event) {
     event.dataTransfer.setData("task", event.target.id);
 });
-
+// track when task card is moving while dragged
+// prevent default to allow card to enter/be dropped into column/drop target
 document.addEventListener("dragover", function (event) {
     event.preventDefault();
 });
-
+// confirms drop target and appends (drops) task to the column
 document.addEventListener("drop", function (event) {
     event.preventDefault();
     if (event.target.className == "taskCol") {
@@ -16,7 +17,7 @@ document.addEventListener("drop", function (event) {
     }
 });
 
-// delete tasks
+// finds and gives option to delete all elements with class delete within the task divs
 const taskDelete = document.querySelectorAll(".delete");
 
 taskDelete.forEach((x) => {
@@ -25,8 +26,8 @@ taskDelete.forEach((x) => {
     });
 });
 
-// Add task details popup
-// https://www.w3schools.com/howto/howto_css_modals.asp
+// Add task details popup/modal
+// display was set to none with css - here changes display so the modal can pop up
 const popup = document.getElementById("taskDetails");
 const addTask = document.getElementById("addTask");
 const popupClose = document.getElementsByClassName("popupClose")[0];
@@ -45,20 +46,21 @@ const taskSubmit = document.getElementById("taskSubmit");
 taskSubmit.addEventListener("click", createTask);
 
 function createTask() {
+    // create new task div
     const taskDiv = document.createElement("div");
-
+    // get all values from user input
     const taskTitle_val = document.getElementById("taskTitle").value;
     const taskDesc_val = document.getElementById("taskDesc").value;
     const taskPriority_val = document.getElementById("taskPriority").value;
     const taskDueDate_val = document.getElementById("taskDueDate").value;
     const taskTime_val = document.getElementById("taskTime").value;
-
+    // create text of user input
     const taskTitle = document.createTextNode(taskTitle_val);
     const taskDesc = document.createTextNode(taskDesc_val);
     const taskPriority = document.createTextNode(taskPriority_val);
     const taskDueDate = document.createTextNode(taskDueDate_val);
     const taskTime = document.createTextNode(taskTime_val);
-
+    // append all created text to new task div with line break separating each line of text
     taskDiv.appendChild(taskTitle);
     const br = document.createElement("br");
     taskDiv.appendChild(br);
@@ -76,16 +78,15 @@ function createTask() {
 
     taskDiv.classList.add("task");
     taskDiv.setAttribute("draggable", "true");
-
+    // create delete option in new task div
     const close = document.createElement("span");
     const closeIcon = document.createTextNode("\u00D7");
     close.classList.add("delete");
     close.appendChild(closeIcon);
 
     taskDiv.appendChild(close);
-
     noStatus.appendChild(taskDiv);
-
+    // event listener deletes task div
     close.addEventListener("click", () => {
         close.parentElement.style.display = "none";
     });
@@ -119,31 +120,34 @@ function createTask() {
 document.getElementById("addCol").addEventListener("click", addColumn);
 
 function addColumn() {
+    // create new column div
     var newColumn = document.createElement("div");
     newColumn.style.width = "85%";
-
+    // create new status element with existing styles from css
     var newStatus = document.createElement("h3");
     newStatus.style.cssText += "padding:10px;background-color:white;color:#323232";
     newStatus.style.borderRadius = "10px";
     newStatus.style.cssText += "display:flex;justify-content:center";
-
-    var headingText = document.createTextNode("new column");
-
+    // heading for new column
+    var headingText = document.createTextNode("Status");
+    // create new task column div with existing styles from css
     var newTaskCol = document.createElement("div");
     newTaskCol.style.cssText += "margin-top:10px;padding:10px;width:auto;height:90%;background-color:#606060";
     newTaskCol.style.borderRadius = "10px";
+    // get kanban board div
+    var kanbanBoard = document.getElementById("kanbanBoard");
 
-    var kBoard = document.getElementById("kanbanBoard");
-
+    // append all newly created elements together
     newStatus.appendChild(headingText);
     newColumn.appendChild(newStatus);
     newColumn.appendChild(newTaskCol);
-    kBoard.appendChild(newColumn);
+    // and finally append to the big kanban board div
+    kanbanBoard.appendChild(newColumn);
 }
 
-// open and close sidebar functions following W3 School How TO - Collapse Sidebar Tutorial:
-// https://www.w3schools.com/howto/howto_js_collapse_sidebar.asp 
-// REMEMBER TO APA REFERENCE^^^
+// features sliding panel
+// display for sliding panel "features" was to 0 in css
+// these functions make the div expand and shrink back to 0
 function openMusic() {
     document.querySelector(".features").style.width = "300px";
 }
@@ -153,7 +157,7 @@ function closeFeat() {
 }
 
 // music player 
-
+// get all relevant nodes from html
 const nowPlaying = document.querySelector('.nowPlaying');
 const songCover = document.querySelector('.songCover');
 const songTitle = document.querySelector('.songTitle');
@@ -167,70 +171,73 @@ const musicSeeker = document.querySelector('.musicSeeker');
 const audio = document.getElementById('audio');
 const currentSong = document.createElement('audio');
 
+// define the index of songs and which to start from in the array
 let song_index = 0;
 let songPlaying = false;
-let updateTimer;
 
+// create variable storing songs details in an array to be called
 const songs = [
     {
-        img : 'music/shelter.jpg',
-        music : 'music/shelter.mp3',
-        name : 'Shelter',
-        artist : 'Madeon'
+        img: 'music/shelter.jpg',
+        music: 'music/shelter.mp3',
+        name: 'Shelter',
+        artist: 'Madeon'
     },
     {
-        img : 'images/down.jpg',
-        music : 'music/Down (feat. Grey).mp3',
-        name : 'Falling Down',
-        artist : 'Ace (feat. Grey)'
+        img: 'images/down.jpg',
+        music: 'music/Down (feat. Grey).mp3',
+        name: 'Falling Down',
+        artist: 'Ace (feat. Grey)'
     }
 ];
 
 loadSong(song_index);
 
 function loadSong(song_index) {
-  currentSong.src = songs[song_index].music;
-  currentSong.load();
+    currentSong.src = songs[song_index].music;
+    currentSong.load();
 
     songCover.style.backgroundImage = "url(" + songs[song_index].img + ")";
     songTitle.textContent = songs[song_index].name;
     songArtist.textContent = songs[song_index].artist;
 }
 
-function songPlayPause(){
+// play and pause songs, add/remove to switch between play and pause icons
+function songPlayPause() {
     songPlaying ? pauseSong() : playSong();
 }
 
 function playSong() {
-  currentSong.play();
-  songPlaying = true;
-  iconPlayPause.innerHTML = '<i class="bx bx-pause bx-lg"></i>';
+    currentSong.play();
+    songPlaying = true;
+    iconPlayPause.innerHTML = '<i class="bx bx-pause bx-lg"></i>';
 }
 
 function pauseSong() {
-  currentSong.pause();
-  songPlaying = false;
-  iconPlayPause.innerHTML = '<i class="bx bx-play bx-lg"></i>';
+    currentSong.pause();
+    songPlaying = false;
+    iconPlayPause.innerHTML = '<i class="bx bx-play bx-lg"></i>';
 }
 
+// tracks length of song index (increase or decrease) in order to move onto other songs 
 function prevSong() {
-  song_index--;
+    song_index--;
 
-  if (song_index < 0) {
-    song_index = songs.length - 1;
-  }
+    if (song_index < 0) {
+        song_index = songs.length - 1;
+    }
 
-  loadSong(song_index);
-  playSong();
+    loadSong(song_index);
+    playSong();
 }
 
 function nextSong() {
-  song_index++;
+    song_index++;
 
-  if (song_index > songs.length - 1) {
-    song_index = 0;
-  }
+    if (song_index > songs.length - 1) {
+        song_index = 0;
+    }
 
-  loadSong(song_index);
-  playSong();
+    loadSong(song_index);
+    playSong();
 }
